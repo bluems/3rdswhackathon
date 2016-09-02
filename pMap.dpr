6cd43bfa5d771bcdca1cd4503b75deb687b12960
nazuna1 <nazuna@nazuna.kr>
2016-09-02 20:45:21 +0900

    초기 파일 업로드

---
+++ pMap.dpr
@@ -0,0 +1,14 @@
+program pMap;
+
+uses
+  Vcl.Forms,
+  uMap in 'uMap.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
